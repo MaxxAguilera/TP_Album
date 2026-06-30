276cc8567391450aab4321a5054be915f1914968
MÁXIMO AGUILERA <49628407@ort>
2026-06-30 09:06:25 -0300

    first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace TP_Album_Aguilera_Perez_Tortosa.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
